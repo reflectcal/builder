b18ce8049099158e1506e9fba3c20316042aa659
alexeykomov <alexeykofficial@gmail.com>
2017-08-17 00:36:44 +0300

    edit: structure.

--- build-web.sh
+++ build-web.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
-cd ./build
-git clone https://github.com/reflectcal/reflectcal
-cd ./reflectcal
-grunt build
-cd ..
 
-git clone https://github.com/reflectcal/web-server-container
-docker build web-server-container
+# instead of rm -rf
+#mv -f target/ /tmp/.
+#mkdir target
+#mkdir target/reflectcal
+#git clone https://github.com/reflectcal/reflectcal target/reflectcal
+#cd target/reflectcal
+#npm install
+#grunt build --dev
+#cd ../..
 
-git clone https://github.com/reflectcal/app-server-container
-docker build app-server-container
+#mkdir target/web-server-container
+#git clone https://github.com/reflectcal/web-server-container target/web-server-container
+#сp target/reflectcal/build/static/* target/web-server-container/src/
+#docker build web-server-container
+
+#mkdir target/app-server-container
+#git clone https://github.com/reflectcal/app-server-container target/app-server-container
+cp -r target/reflectcal/build/app target/app-server-container/src/
+cp -r target/reflectcal/build/logs target/app-server-container/src/
+cp -r target/reflectcal/build/app.js target/app-server-container/src/
+cp -r target/reflectcal/build/package.json target/app-server-container/src/
+
+docker build --tag reflectcal/app-server:latest target/app-server-container
 
