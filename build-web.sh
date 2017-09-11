7800553824a51db020d81038662cade6ca521606
alexeykomov <alexeykofficial@gmail.com>
2017-09-11 09:44:03 +0300

    add: building script

--- build-web.sh
+++ build-web.sh
@@ -2,7 +2,7 @@
 
 # Building web-application before containers
 # instead of rm -rf
-mv -f target/ /tmp/.
+rm -rf target
 mkdir target
 mkdir target/reflectcal
 git clone https://github.com/reflectcal/reflectcal target/reflectcal
@@ -14,8 +14,10 @@ cd ../..
 # Building web server
 mkdir target/web-server-container
 git clone https://github.com/reflectcal/web-server-container target/web-server-container
-сp target/reflectcal/build/static/* target/web-server-container/src/
+cp -r target/reflectcal/build/static target/web-server-container/src/
+cp ~/.ssh/id_rsa_cont.pub target/web-server-container/id_rsa_cont.pub
 docker build --tag reflectcal/web-server:latest target/web-server-container
+docker push reflectcal/web-server
 
 # Building app server
 mkdir target/app-server-container
@@ -24,18 +26,28 @@ cp -r target/reflectcal/build/app target/app-server-container/src/
 cp -r target/reflectcal/build/logs target/app-server-container/src/
 cp target/reflectcal/build/app.js target/app-server-container/src/
 cp target/reflectcal/build/package.json target/app-server-container/src/
-cp client_secret_* target/app-server-container/src/
-
+cp ~/.ssh/id_rsa_cont.pub target/app-server-container/id_rsa_cont.pub
 docker build --tag reflectcal/app-server:latest target/app-server-container
+docker push reflectcal/app-server
 
 # Building mailer daemon
+mkdir target/reflectcal-mailer
+git clone https://github.com/reflectcal/reflectcal-mailer target/reflectcal-mailer
 mkdir target/mail-daemon-container
 git clone https://github.com/reflectcal/mail-daemon-container target/mail-daemon-container
-cp -r target/reflectcal/build/app target/mail-daemon-container/src/
-cp -r target/reflectcal/build/logs target/mail-daemon-container/src/
-cp target/reflectcal/build/app.js target/mail-daemon-container/src/
-cp target/reflectcal/build/package.json target/mail-daemon-container/src/
-cp client_secret_* target/mail-daemon-container/src/
 
-docker build --tag reflectcal/app-server:latest target/mail-daemon-container
+cd target/reflectcal-mailer
+npm install
+gulp babel
+cd ../..
+
+cp -r target/reflectcal-mailer/js target/mail-daemon-container/src/
+cp -r target/reflectcal-mailer/src target/mail-daemon-container/src/
+cp -r target/reflectcal-mailer/templates target/mail-daemon-container/src/
+cp -r target/reflectcal-mailer/logs target/mail-daemon-container/src/
+cp target/reflectcal-mailer/package.json target/mail-daemon-container/src/
+
+cp ~/.ssh/id_rsa_cont.pub target/mail-daemon-container/id_rsa_cont.pub
+docker build --tag reflectcal/mail-daemon:latest target/mail-daemon-container
+docker push reflectcal/mail-daemon
 
